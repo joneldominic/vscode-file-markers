20c2ee60aacb31e240b99b8f3269d1d36fcc9d65
Jonel Dominic Tapang <jonel@skand.io>
2026-02-01 23:51:04 +0800

    feat: add automated versioning with release-please and conventional commits

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New feature
+        'fix',      // Bug fix
+        'docs',     // Documentation
+        'style',    // Formatting, no code change
+        'refactor', // Code change that neither fixes a bug nor adds a feature
+        'perf',     // Performance improvement
+        'test',     // Adding tests
+        'chore',    // Maintenance tasks
+        'ci',       // CI/CD changes
+        'build',    // Build system changes
+        'revert',   // Revert previous commit
+      ],
+    ],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+  },
+};
